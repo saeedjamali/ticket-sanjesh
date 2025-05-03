da6ac79a1ea64b07171b804a24d4f41f566460d6
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2025-05-03 08:20:14 +0330

    add image uploader

--- src/app/api/direct-login/route.js
+++ src/app/api/direct-login/route.js
@@ -49,7 +49,7 @@ export async function POST(request) {
     const addRefreshTokenToUser = await User.findByIdAndUpdate(user._id, {
       refreshToken,
     });
-    console.log("Refresh token added to user", refreshToken);
+    console.log("addRefreshTokenToUser token added to user", addRefreshTokenToUser);
     // Set cookies
     const cookieStore = cookies();
     cookieStore.set("access-token", accessToken, {
